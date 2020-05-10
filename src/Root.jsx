97b2e52600f89481bcf594a4a94ec2a8082bbe7a
afrianjunior <afrian.junior26@gmail.com>
2020-05-10 13:43:26 +0700

    feat: install styletron and atomize

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import client from '@/lib/gql-client'
 import { ApolloProvider } from '@apollo/react-hooks'
+import { StyleReset } from 'atomize'
+import { Provider as StyletronProvider } from 'styletron-react'
+import { Client as Styletron } from 'styletron-engine-atomic'
 
 import App from './app/App'
 
+const engine = new Styletron()
+
 export default function Root () {
   return (
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <StyletronProvider value={engine}>
+      <ApolloProvider client={client}>
+        <StyleReset />
+        <App />
+      </ApolloProvider>
+    </StyletronProvider>
   )
 }
