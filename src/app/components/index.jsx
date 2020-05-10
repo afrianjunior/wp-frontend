4d2453f2f473b9c2d01a344eeb59413bc9d733cc
afrianjunior <afrian.junior26@gmail.com>
2020-05-10 15:29:59 +0700

    feat: register label component

--- src/app/components/index.jsx
+++ src/app/components/index.jsx
@@ -1,9 +1,11 @@
 import Pokemons from './Pokemons'
 import PokemonCard from './PokemonCard'
+import Label from './Label'
 import Error from './Error'
 
 export {
   Pokemons,
   PokemonCard,
+  Label,
   Error
 }
