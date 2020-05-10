ad5fa09ebb8c05ce2b91561b1827857f67446ce4
afrianjunior <afrian.junior26@gmail.com>
2020-05-10 13:48:00 +0700

    feat: add pokemont list and graphql call

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import POKEMONS_QUERY from '@/gql/pokemonsQuery'
+import { Div, Text } from 'atomize'
+import { Pokemons } from '@/app/components'
 
 function Home () {
+  const [pokemonsVariables] = useState({ first: 20 })
+  const { loading, error, data, fetchMore } = useQuery(POKEMONS_QUERY, {
+    variables: pokemonsVariables
+  })
+
   return (
     <>
-      Hello
+      <Div
+        m={{ b: '40px' }}
+        textAlign="center"
+      >
+        <Text tag="h1" textSize="display2">Pokedex</Text>
+      </Div>
+      {loading ? 'Fetching...' : <Pokemons {...data} />}
     </>
   )
 }
